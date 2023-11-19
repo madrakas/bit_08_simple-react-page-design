e620431b11ff24d3683dedae06575a7676da4143
Arvydas <arvydas.simbelis@gmail.com>
2023-11-19 16:34:09 +0200

    Middle stat row

---
+++ src/components/statdetail/StatDetail.jsx
@@ -0,0 +1,9 @@
+import style from './StatDetail.module.css';
+
+export function StatDetail(){
+    return(
+        <div className={style.statContainer}>
+            <h2> Stat detail</h2>
+        </div>
+    );
+}
--- src/components/topstat/TopStat.jsx
+++ src/components/topstat/TopStat.jsx
@@ -3,10 +3,10 @@ import style from './TopStat.module.css';
 
 export function TopStat (){
     const statItems= [
-    <StatItem title='Total Revenue' icon='revenue' number='216k' growth='$341' prefix='$' />,
-    <StatItem title='Invoices' icon='invoices' number='2,221' growth='121'/>,
-    <StatItem title='Clients' icon='clients' number='1,423' growth='91'/>,
-    <StatItem title='Loyality' icon='loyality' number='78%' negative='true' growth='1%'/>];
+    <StatItem key={1} title='Total Revenue' icon='revenue' number='216k' growth='$341' prefix='$' />,
+    <StatItem key={2} title='Invoices' icon='invoices' number='2,221' growth='121'/>,
+    <StatItem key={3} title='Clients' icon='clients' number='1,423' growth='91'/>,
+    <StatItem key={4} title='Loyality' icon='loyality' number='78%' negative='true' growth='1%'/>];
     return(
         <div className={style.topStat}>
             {statItems}        
